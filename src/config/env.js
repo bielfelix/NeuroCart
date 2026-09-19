56f896547390f36d79422184c233f566640e154e
Gabriel Felix <55044310+bielfelix@users.noreply.github.com>
2026-09-19 05:37:35 -0300

    fix: use localhost as safe default MongoDB endpoint

--- src/config/env.js
+++ src/config/env.js
@@ -2,6 +2,6 @@ import 'dotenv/config';
 
 export const config = {
   port: process.env.PORT || 3000,
-  mongoUri: process.env.MONGODB_URI || 'mongodb://192.168.0.128:27017',
+  mongoUri: process.env.MONGODB_URI || 'mongodb://localhost:27017',
   mongoDbName: process.env.MONGODB_DB_NAME || 'ecommerce-aula',
 };
